8f9c69bf4c39b30febfe4fcab85930c2804ebbac
Leonardo Fontes <leofontesrodrigues@hotmail.com>
2024-04-28 18:27:34 -0300

    Envio do desafio

---
+++ store/controllers/product.py
@@ -0,0 +1,54 @@
+from typing import List
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query, status
+from pydantic import UUID4
+from store.core.exceptions import NotFoundException, InsertionException
+from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
+from store.usecases.product import ProductUsecase
+
+router = APIRouter(tags=["products"])
+
+@router.post(path="/", status_code=status.HTTP_201_CREATED)
+async def post(
+    body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    try:
+        return await usecase.create(body=body)
+    except InsertionException as exc:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc))
+
+@router.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    try:
+        return await usecase.get(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
+
+@router.get(path="/", status_code=status.HTTP_200_OK)
+async def query(
+    min_price: float = Query(None),
+    max_price: float = Query(None),
+    usecase: ProductUsecase = Depends()
+) -> List[ProductOut]:
+    return await usecase.query(min_price=min_price, max_price=max_price)
+
+@router.patch(path="/{id}", status_code=status.HTTP_200_OK)
+async def patch(
+    id: UUID4 = Path(alias="id"),
+    body: ProductUpdate = Body(...),
+    usecase: ProductUsecase = Depends(),
+) -> ProductUpdateOut:
+    try:
+        return await usecase.update(id=id, body=body)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+@router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> None:
+    try:
+        await usecase.delete(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
